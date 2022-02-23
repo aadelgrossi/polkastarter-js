893fcc5c58d531697c01a78d7f3d4291ab4af8c0
Andre Grossi <iamandregrossi@gmail.com>
2022-02-23 15:41:11 -0300

    refactor(src/utils): Lint and fix src/utils folder and files

--- src/utils/Chains.ts
+++ src/utils/Chains.ts
@@ -1,45 +1,35 @@
-
 const ETH_URL_MAINNET =
-  "https://mainnet.infura.io/v3/40e2d4f67005468a83e2bcace6427bc8";
+  'https://mainnet.infura.io/v3/40e2d4f67005468a83e2bcace6427bc8';
 const ETH_URL_TESTNET =
-  "https://kovan.infura.io/v3/40e2d4f67005468a83e2bcace6427bc8";
-const MOONBEAM_URL =
-  "https://rpc.api.moonbeam.network";
-const MOONBEAM_TESTNET_URL =
-  "https://rpc.api.moonbase.moonbeam.network";
+  'https://kovan.infura.io/v3/40e2d4f67005468a83e2bcace6427bc8';
+const MOONBEAM_URL = 'https://rpc.api.moonbeam.network';
+const MOONBEAM_TESTNET_URL = 'https://rpc.api.moonbase.moonbeam.network';
 const BINANCE_CHAIN_TESTNET_URL =
-  "https://data-seed-prebsc-1-s1.binance.org:8545";
-const BINANCE_CHAIN_URL =
-  "https://bsc-dataseed1.binance.org:443";
-const POLYGON_CHAIN_TESTNET_URL =
-  "https://rpc-mumbai.maticvigil.com/";
-const POLYGON_CHAIN_URL =
-  "https://rpc-mainnet.maticvigil.com/";
-const CELO_CHAIN_URL =
-  "https://forno.celo.org";
-const CELO_CHAIN_TESTNET_URL =
-  "https://alfajores-forno.celo-testnet.org";
-const AVAX_CHAIN_URL =
-  "https://api.avax.network/ext/bc/C/rpc";
-const AVAX_CHAIN_TESTNET_URL =
-  "https://api.avax-test.network/ext/bc/C/rpc";
+  'https://data-seed-prebsc-1-s1.binance.org:8545';
+const BINANCE_CHAIN_URL = 'https://bsc-dataseed1.binance.org:443';
+const POLYGON_CHAIN_TESTNET_URL = 'https://rpc-mumbai.maticvigil.com/';
+const POLYGON_CHAIN_URL = 'https://rpc-mainnet.maticvigil.com/';
+const CELO_CHAIN_URL = 'https://forno.celo.org';
+const CELO_CHAIN_TESTNET_URL = 'https://alfajores-forno.celo-testnet.org';
+const AVAX_CHAIN_URL = 'https://api.avax.network/ext/bc/C/rpc';
+const AVAX_CHAIN_TESTNET_URL = 'https://api.avax-test.network/ext/bc/C/rpc';
 
 const networksEnum = Object.freeze({
-  1: "Ethereum Main",
-  2: "Morden",
-  3: "Ropsten",
-  4: "Rinkeby",
-  56: "BSC Main",
-  97: "BSC Test",
-  42: "Kovan",
-  137: "Polygon",
-  80001: "Mumbai",
-  44787: "Celo Testnet",
-  42220: "Celo",
-  43114: "Avalanche",
-  43113: "Avalanche Testnet",
-  1284: "Moonbeam",
-  1287: "Moonbeam Testnet"
+  1: 'Ethereum Main',
+  2: 'Morden',
+  3: 'Ropsten',
+  4: 'Rinkeby',
+  56: 'BSC Main',
+  97: 'BSC Test',
+  42: 'Kovan',
+  137: 'Polygon',
+  80001: 'Mumbai',
+  44787: 'Celo Testnet',
+  42220: 'Celo',
+  43114: 'Avalanche',
+  43113: 'Avalanche Testnet',
+  1284: 'Moonbeam',
+  1287: 'Moonbeam Testnet',
 });
 
 /**
@@ -47,55 +37,56 @@ const networksEnum = Object.freeze({
  * @constructor Chains
  */
 class Chains {
-  constructor() {}
-
   private readonly networksEnum = Object.freeze({
-    1: "Ethereum Main",
-    2: "Morden",
-    3: "Ropsten",
-    4: "Rinkeby",
-    56: "BSC Main",
-    97: "BSC Test",
-    42: "Kovan",
-    137: "Polygon",
-    80001: "Mumbai",
-    44787: "Celo Testnet",
-    42220: "Celo",
-    43114: "Avalanche",
-    43113: "Avalanche Testnet",
-    1284: "Moonbeam",
-    1287: "Moonbeam Testnet"
+    1: 'Ethereum Main',
+    2: 'Morden',
+    3: 'Ropsten',
+    4: 'Rinkeby',
+    56: 'BSC Main',
+    97: 'BSC Test',
+    42: 'Kovan',
+    137: 'Polygon',
+    80001: 'Mumbai',
+    44787: 'Celo Testnet',
+    42220: 'Celo',
+    43114: 'Avalanche',
+    43113: 'Avalanche Testnet',
+    1284: 'Moonbeam',
+    1287: 'Moonbeam Testnet',
   });
 
   checkIfNetworkIsSupported = (network: string) => {
-    if(!this.isNetworkSupported(network)) {
-			throw new Error("Network has to be ETH, DOT, BSC, MATIC, CELO or AVAX");
-		}
-  }
+    if (!this.isNetworkSupported(network)) {
+      throw new Error('Network has to be ETH, DOT, BSC, MATIC, CELO or AVAX');
+    }
+  };
 
   isNetworkSupported = (network: string) => {
-    return ['ETH', 'DOT','BSC','MATIC','CELO', 'AVAX'].includes(network)
-  }
+    return ['ETH', 'DOT', 'BSC', 'MATIC', 'CELO', 'AVAX'].includes(network);
+  };
 
   getRpcUrl = (network: string, mainnet = true) => {
-    if(network == 'DOT') {
+    if (network === 'DOT') {
       return mainnet ? MOONBEAM_URL : MOONBEAM_TESTNET_URL;
-		} else if(network == 'BSC') {
-			return mainnet ? BINANCE_CHAIN_URL : BINANCE_CHAIN_TESTNET_URL;
-		} else if(network == 'ETH') {
-			return mainnet ? ETH_URL_MAINNET : ETH_URL_TESTNET;
-		} else if(network == 'MATIC') {
-			return mainnet ? POLYGON_CHAIN_URL : POLYGON_CHAIN_TESTNET_URL;
-		} else if(network == 'CELO') {
-			return mainnet ? CELO_CHAIN_URL : CELO_CHAIN_TESTNET_URL;
-		} else if(network == 'AVAX') {
-			return mainnet ? AVAX_CHAIN_URL : AVAX_CHAIN_TESTNET_URL;
-		}
-  }
+    }
+    if (network === 'BSC') {
+      return mainnet ? BINANCE_CHAIN_URL : BINANCE_CHAIN_TESTNET_URL;
+    }
+    if (network === 'ETH') {
+      return mainnet ? ETH_URL_MAINNET : ETH_URL_TESTNET;
+    }
+    if (network === 'MATIC') {
+      return mainnet ? POLYGON_CHAIN_URL : POLYGON_CHAIN_TESTNET_URL;
+    }
+    if (network === 'CELO') {
+      return mainnet ? CELO_CHAIN_URL : CELO_CHAIN_TESTNET_URL;
+    }
+    if (network === 'AVAX') {
+      return mainnet ? AVAX_CHAIN_URL : AVAX_CHAIN_TESTNET_URL;
+    }
+  };
 
   getNetworksEnum = () => networksEnum;
 }
 
-export default new Chains()
-
-
+export default new Chains();
--- src/utils/Signer.ts
+++ src/utils/Signer.ts
@@ -1,12 +1,13 @@
 import Decimal from 'decimal.js';
 import * as ethers from 'ethers';
-import Numbers from "./Numbers";
+
+import Numbers from './Numbers';
 
 /**
  * @typedef Signer
  * @type {object}
  * @property {Function} signMessage - Signs a message
-*/
+ */
 
 /**
  * @typedef SignedAddress
@@ -14,243 +15,256 @@ import Numbers from "./Numbers";
  * @property {string} address - Address.
  * @property {string} allocation - Max Allocation.
  * @property {string} signature - Signed Address
-*/
+ */
 
 type EthersSigner = ethers.ethers.Wallet;
 
-type GenerateSignerAccountArgs = { password: string, entropy?: string }
-type AddressFromAccountArgs = { accountJson: string, password: string }
+type GenerateSignerAccountArgs = { password: string; entropy?: string };
+type AddressFromAccountArgs = { accountJson: string; password: string };
 
 type SignAddressesArgs = {
-    addresses: string[],
-    accountJson: string
-    accountMaxAllocations: Decimal[],
-    decimals: number,
-    contractAddress: string;
-    password: string;
-}
+  addresses: string[];
+  accountJson: string;
+  accountMaxAllocations: Decimal[];
+  decimals: number;
+  contractAddress: string;
+  password: string;
+};
 
 type VerifySignatureArgs = {
-    signature: string,
-    address: string,
-    accountMaxAllocation: string,
-    contractAddress: string,
-    signerAddress: string
-}
+  signature: string;
+  address: string;
+  accountMaxAllocation: string;
+  contractAddress: string;
+  signerAddress: string;
+};
 
 type SignAddressesWithSignerArgs = {
-    addresses: string[],
-    accountJson: string
-    accountMaxAllocations: Decimal[],
-    decimals: number,
-    contractAddress: string;
-    password: string;
-    signer: EthersSigner;
-}
+  addresses: string[];
+  accountJson: string;
+  accountMaxAllocations: Decimal[];
+  decimals: number;
+  contractAddress: string;
+  password: string;
+  signer: EthersSigner;
+};
 
 type SignAddressArgs = {
-    signer: EthersSigner,
-    address: string;
-    accountMaxAllocation: string;
-    contractAddress: string
-}
+  signer: EthersSigner;
+  address: string;
+  accountMaxAllocation: string;
+  contractAddress: string;
+};
 
-type SignMessageArgs = { signer: EthersSigner, message: ethers.Bytes | string }
+type SignMessageArgs = { signer: EthersSigner; message: ethers.Bytes | string };
 
 /**
  * Signer object
  * @constructor Signer
-*/
+ */
 class Signer {
+  /**
+   * @function generateSignerAccount
+   * @description Generates a new private key for signing the whitelist addresses
+   * @param {string} password Password for encryption
+   * @param {string=} entropy Add more entropy
+   * @returns {string} JSON Account
+   */
+  generateSignerAccount = async ({
+    password,
+    entropy,
+  }: GenerateSignerAccountArgs) => {
+    const wallet = ethers.Wallet.createRandom(entropy);
+    return wallet.encrypt(password);
+  };
 
-    /**
-	 * @function generateSignerAccount
-	 * @description Generates a new private key for signing the whitelist addresses
-	 * @param {string} password Password for encryption
-	 * @param {string=} entropy Add more entropy
-     * @returns {string} JSON Account
-	 */
-    generateSignerAccount = async ({ password, entropy }: GenerateSignerAccountArgs) => {
-        const wallet = ethers.Wallet.createRandom(entropy)
-        return await wallet.encrypt(password);
-    }
+  /**
+   * @function getAddressFromAccount
+   * @description Recovers an account given a json file
+   * @param {string} accountJson Account in a json format
+   * @param {string} password Password to unlock the account
+   * @returns {string} Address
+   */
+  getAddressFromAccount = ({
+    accountJson,
+    password,
+  }: AddressFromAccountArgs) => {
+    const signer = ethers.Wallet.fromEncryptedJsonSync(accountJson, password);
+    return signer.address;
+  };
 
-    /**
-	 * @function getAddressFromAccount
-	 * @description Recovers an account given a json file
-     * @param {string} accountJson Account in a json format
-     * @param {string} password Password to unlock the account
-     * @returns {string} Address
-	 */
-     getAddressFromAccount = ({ accountJson, password }: AddressFromAccountArgs) => {
-        const signer = ethers.Wallet.fromEncryptedJsonSync(
-            accountJson,
-            password
-        );
-        return signer.address;
-    }
+  /**
+   * @function signAddresses
+   * @description Signs an array of addresses. Will ignore malformed addresses.
+   * @param {string[]} addresses List of addresses to sign
+   * @param {string} accountJson Account in a json format
+   * @param {number[]} accountMaxAllocations List of mac allocations in wei
+   * @param {number} decimals Decimals for the max allocation
+   * @param {string} contractAddress Pool
+   * @param {string} password Password to unlock the account
+   * @returns {SignedAddress[]} signedAddresses
+   */
+  signAddresses = async ({
+    addresses,
+    accountJson,
+    password,
+    accountMaxAllocations,
+    contractAddress,
+    decimals,
+  }: SignAddressesArgs) => {
+    const signer = ethers.Wallet.fromEncryptedJsonSync(accountJson, password);
 
-    /**
-	 * @function signAddresses
-	 * @description Signs an array of addresses. Will ignore malformed addresses.
-	 * @param {string[]} addresses List of addresses to sign
-     * @param {string} accountJson Account in a json format
-	 * @param {number[]} accountMaxAllocations List of mac allocations in wei
-	 * @param {number} decimals Decimals for the max allocation
-	 * @param {string} contractAddress Pool
-     * @param {string} password Password to unlock the account
-     * @returns {SignedAddress[]} signedAddresses
-	 */
-    signAddresses = async ({
-        addresses,
-        accountJson,
-        password,
-        accountMaxAllocations,
-        contractAddress,
-        decimals }: SignAddressesArgs) => {
-        const signer = ethers.Wallet.fromEncryptedJsonSync(
-            accountJson,
-            password
-        );
+    return this.signAddressesWithSigner({
+      addresses,
+      accountMaxAllocations,
+      contractAddress,
+      decimals,
+      accountJson,
+      password,
+      signer,
+    });
+  };
 
-        return await this.signAddressesWithSigner({
-            addresses,
-            accountMaxAllocations,
-            contractAddress,
-            decimals,
-            accountJson,
-            password,
-            signer
-        });
-    }
+  /**
+   * @function signAddressesWithSigner
+   * @description Signs an array of addresses. Will ignore malformed addresses.
+   * @param {string[]} addresses List of addresses to sign
+   * @param {number[]} accountMaxAllocations List of mac allocations in wei
+   * @param {number} decimals Decimals for the max allocation
+   * @param {string} contractAddress Pool
+   * @param {Signer} signer Signer object
+   * @returns {SignedAddress[]} signedAddresses
+   */
+  signAddressesWithSigner = async ({
+    addresses,
+    accountMaxAllocations,
+    contractAddress,
+    decimals,
+    signer,
+  }: SignAddressesWithSignerArgs) => {
+    const signedAddresses = [];
+    let processed = 0;
+    let rejected = 0;
 
-    /**
-	 * @function signAddressesWithSigner
-	 * @description Signs an array of addresses. Will ignore malformed addresses.
-	 * @param {string[]} addresses List of addresses to sign
-	 * @param {number[]} accountMaxAllocations List of mac allocations in wei
-	 * @param {number} decimals Decimals for the max allocation
-	 * @param {string} contractAddress Pool
-     * @param {Signer} signer Signer object
-     * @returns {SignedAddress[]} signedAddresses
-	 */
-    signAddressesWithSigner = async ({
-        addresses,
-        accountMaxAllocations,
-        contractAddress,
-        decimals,
-        signer
-    }: SignAddressesWithSignerArgs) => {
-        const signedAddresses = [];
-        let processed = 0;
-        let rejected = 0;
-
-        addresses.map(async (address, index) => {
-            const allocation = Numbers.toSmartContractDecimals(
-                accountMaxAllocations[index],
-                decimals
-            );
-            const result = await this._trySignAddress(signer, address, allocation, contractAddress);
-            if (result) {
-                signedAddresses.push({
-                    address,
-                    signature: result,
-                    allocation: allocation,
-                });
-                processed++;
-            } else {
-                rejected++;
-            }
-        })
-
-        console.info(processed, "lines successfully processed");
-        console.info(rejected, "lines rejected");
-        return signedAddresses;
-    }
-
-    /**
-	 * @function signMessage
-	 * @description Signs a message given an account
-	 * @param {Signer} signer Signer
-	 * @param {string} message String to sign
-     * @returns {string} signedString
-	 */
-    signMessage = async ({ signer, message }: SignMessageArgs) => {
-        return await signer.signMessage(message);
-    }
-
-    /**
-	 * @function verifySignature
-	 * @description Verifies if an address has been signed with the signer address
-     * @param {string} signature Signature
-	 * @param {string} address Address signed
-	 * @param {string} contractAddress Pool contract address
-	 * @param {string} accountMaxAllocation Max allocation
-	 * @param {string} signerAddress Address who signed the message
-     * @returns {boolean} verified
-	 */
-    verifySignature = async ({
-        signature,
+    addresses.map(async (address, index) => {
+      const allocation = Numbers.toSmartContractDecimals(
+        accountMaxAllocations[index],
+        decimals
+      );
+      const result = await this._trySignAddress(
+        signer,
         address,
-        accountMaxAllocation,
-        contractAddress,
-        signerAddress }: VerifySignatureArgs) => {
-        try {
-            const actualAddress = ethers.utils.verifyMessage(this._addressToBytes32(address, accountMaxAllocation, contractAddress), signature);
-            return signerAddress.toLowerCase() === actualAddress.toLowerCase();
-        } catch (e) {
-            return false;
-        }
+        allocation,
+        contractAddress
+      );
+      if (result) {
+        signedAddresses.push({
+          address,
+          signature: result,
+          allocation,
+        });
+        processed += 1;
+      } else {
+        rejected += 1;
+      }
+    });
+
+    console.info(processed, 'lines successfully processed');
+    console.info(rejected, 'lines rejected');
+    return signedAddresses;
+  };
+
+  /**
+   * @function signMessage
+   * @description Signs a message given an account
+   * @param {Signer} signer Signer
+   * @param {string} message String to sign
+   * @returns {string} signedString
+   */
+  signMessage = async ({ signer, message }: SignMessageArgs) => {
+    return signer.signMessage(message);
+  };
+
+  /**
+   * @function verifySignature
+   * @description Verifies if an address has been signed with the signer address
+   * @param {string} signature Signature
+   * @param {string} address Address signed
+   * @param {string} contractAddress Pool contract address
+   * @param {string} accountMaxAllocation Max allocation
+   * @param {string} signerAddress Address who signed the message
+   * @returns {boolean} verified
+   */
+  verifySignature = async ({
+    signature,
+    address,
+    accountMaxAllocation,
+    contractAddress,
+    signerAddress,
+  }: VerifySignatureArgs) => {
+    try {
+      const actualAddress = ethers.utils.verifyMessage(
+        this._addressToBytes32(address, accountMaxAllocation, contractAddress),
+        signature
+      );
+      return signerAddress.toLowerCase() === actualAddress.toLowerCase();
+    } catch (e) {
+      return false;
     }
+  };
 
+  /**
+   * @function signAddress
+   * @description Signs a address given an account
+   * @param {Signer} signer Signer object
+   * @param {string} address Address to sign
+   * @param {string} contractAddress Pool contract address
+   * @param {string} accountMaxAllocation Max allocation
+   * @returns {string} signedString
+   */
+  signAddress = async ({
+    signer,
+    address,
+    accountMaxAllocation,
+    contractAddress,
+  }: SignAddressArgs) => {
+    const message = this._addressToBytes32(
+      address,
+      accountMaxAllocation,
+      contractAddress
+    );
+    return this.signMessage({ signer, message });
+  };
 
-    /**
-	 * @function signAddress
-	 * @description Signs a address given an account
-     * @param {Signer} signer Signer object
-	 * @param {string} address Address to sign
-	 * @param {string} contractAddress Pool contract address
-	 * @param {string} accountMaxAllocation Max allocation
-     * @returns {string} signedString
-	 */
-    signAddress = async ({
+  _trySignAddress = async (
+    signer: EthersSigner,
+    address: string,
+    accountMaxAllocation: string,
+    contractAddress: string
+  ) => {
+    const isAddress = ethers.utils.isAddress(address);
+    const addressBigNumber = ethers.BigNumber.from(address).toNumber();
+
+    if (isAddress && addressBigNumber !== 0) {
+      return this.signAddress({
         signer,
         address,
         accountMaxAllocation,
-        contractAddress}: SignAddressArgs) => {
-        const message = this._addressToBytes32(address, accountMaxAllocation, contractAddress)
-        return await this.signMessage({ signer, message });
+        contractAddress,
+      });
     }
+    console.error('address not valid - ignored :', address);
+    return '';
+  };
 
-    _trySignAddress = async (
-        signer: EthersSigner,
-        address: string,
-        accountMaxAllocation: string,
-        contractAddress: string) => {
-        const isAddress = ethers.utils.isAddress(address)
-        const addressBigNumber = ethers.BigNumber.from(address).toNumber()
+  _addressToBytes32 = (address, accountMaxAllocation, contractAddress) => {
+    const messageHash = ethers.utils.solidityKeccak256(
+      ['address', 'uint256', 'address'],
+      [address, accountMaxAllocation, contractAddress]
+    );
 
-        if (isAddress && addressBigNumber !== 0) {
-            return await this.signAddress({
-                signer,
-                address,
-                accountMaxAllocation,
-                contractAddress
-            });
-        } else {
-          console.error("address not valid - ignored :", address);
-          return "";
-        }
-    }
-
-    _addressToBytes32 = (address, accountMaxAllocation, contractAddress) => {
-        const messageHash = ethers.utils.solidityKeccak256(
-            ["address", "uint256", "address"],
-            [address, accountMaxAllocation, contractAddress],
-        );
-
-        return ethers.utils.arrayify(messageHash);
-    }
+    return ethers.utils.arrayify(messageHash);
+  };
 }
 
 export default Signer;
