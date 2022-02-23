fccd9dc87e18163892534b8a9d19ddb7b4dd41bf
Andre Grossi <iamandregrossi@gmail.com>
2022-02-23 15:38:18 -0300

    refactor(models): Apply linting and type fixes to models

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,49 +1,56 @@
-import Web3 from "web3";
+import Web3 from 'web3';
+import { TransactionReceipt, TransactionConfig } from 'web3-core';
 import { Account as Web3Account } from 'web3-eth-accounts';
-import { TransactionReceipt, TransactionConfig } from 'web3-core'
 
-export interface Account {
-    getBalance: () => Promise<string>
-    getAddress: () => string
-    getPrivateKey: () => string
-    sendEther: (amount: number, address: string, data: string | null) => Promise<TransactionReceipt>
+interface Account {
+  getBalance: () => Promise<string>;
+  getAddress: () => string;
+  getAccount: () => Web3Account;
+  getPrivateKey: () => string;
+  sendEther: (
+    amount: number,
+    address: string,
+    data: string | null
+  ) => Promise<TransactionReceipt>;
 }
 
 class AccountImpl implements Account {
-    private web3: Web3
-    private account: Web3Account;
+  private web3: Web3;
 
-    constructor(web3: Web3, account: Web3Account) {
-        this.web3 = web3;
-        this.account = account;
-    }
+  private account: Web3Account;
 
-    getBalance = async () => {
-        const address = this.getAddress()
-        const wei = await this.web3.eth.getBalance(address);
-        return this.web3.utils.fromWei(wei, 'ether')
-    }
+  constructor(web3: Web3, account: Web3Account) {
+    this.web3 = web3;
+    this.account = account;
+  }
 
-    getAddress = () => this.account.address;
+  getBalance = async () => {
+    const address = this.getAddress();
+    const wei = await this.web3.eth.getBalance(address);
+    return this.web3.utils.fromWei(wei, 'ether');
+  };
 
-    getPrivateKey = () => this.account.privateKey;
+  getAddress = () => this.account.address;
 
-    getAccount = () =>this.account
+  getPrivateKey = () => this.account.privateKey;
 
-    sendEther = async (amount: number, address: string, data: string | null) => {
-        let tx: TransactionConfig = {
-            data: data,
-            from: this.getAddress(),
-            to: address,
-            gas: 443000,
-            value: amount
-        }
-        let result = await this.account.signTransaction(tx);
-        let transaction = await this.web3.eth.sendSignedTransaction(result.rawTransaction);
+  getAccount = () => this.account;
 
-        return transaction;
-    }
+  sendEther = async (amount: number, address: string, data: string | null) => {
+    const tx: TransactionConfig = {
+      data,
+      from: this.getAddress(),
+      to: address,
+      gas: 443000,
+      value: amount,
+    };
+    const result = await this.account.signTransaction(tx);
+    const transaction = await this.web3.eth.sendSignedTransaction(
+      result.rawTransaction
+    );
+
+    return transaction;
+  };
 }
-
 
 export default AccountImpl;
