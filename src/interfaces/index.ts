310fde245d8d8e9ee06a9a2b1d1fa07d2cabf39e
Andre Grossi <iamandregrossi@gmail.com>
2022-02-25 00:19:23 -0300

    refactor(interfaces): with ES6 exports

---
+++ src/interfaces/index.ts
@@ -0,0 +1,14 @@
+import originalJson from './fixedswap.json';
+import oldRedeemMethod from './oldRedeemMethod.json';
+
+const updateFixedSwap = () => {
+  originalJson.abi.push(oldRedeemMethod);
+  return originalJson;
+};
+
+export const fixedswap = updateFixedSwap();
+export { default as fixednftswap } from './fixednftswap.json';
+export { default as fixedswap_legacy } from './fixedswap_legacy.json';
+export { default as ierc20 } from './ierc20token.json';
+export { default as staking } from './staking.json';
+export { default as idostaking } from './idostaking.json';
