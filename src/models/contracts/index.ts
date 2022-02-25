4992561a00edbf3743f0c8f8102ca3cc2f2b3bb9
Andre Grossi <iamandregrossi@gmail.com>
2022-02-25 00:27:02 -0300

    chore(contract): Add named exports for contracts

---
+++ src/models/contracts/index.ts
@@ -0,0 +1,5 @@
+export { default as BaseSwapContract } from './base/BaseSwapContract';
+export { default as FixedNFTSwapContract } from './FixedNFTSwapContract';
+export { default as FixedSwapContract } from './FixedSwapContract';
+export { default as FixedSwapContractLegacy } from './legacy/FixedSwapContractLegacy';
+export { default as IDOStaking } from './IDOStaking';
